25558eb090af402af3dd3981bfe2148a2453aefb
mgtm98 <43015074+mgtm98@users.noreply.github.com>
2018-09-08 03:33:01 +0200

    Update main.py

--- examples/RC Car/main.py
+++ examples/RC Car/main.py
@@ -5,10 +5,10 @@ from machine import Pin
 d4 = Pin(gpio("D4"),Pin.OUT)
 d4.on()
 
-def print_speed(parm,http_object):
+def print_speed(parm,http_object):                                  #TODO code for creating pwm to controll speed
     print(http_object['speed'])
 
-def print_dir(parm,http_object):
+def print_dir(parm,http_object):                                    #TODO code for controlling car direction
     if http_object['dir'] == 'stop' :
         d4.on()
     else:
